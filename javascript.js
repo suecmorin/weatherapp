d003e0421fe1eae39a7c9d36ff1b1acbd265a73a
suecmorin <suecmorin@gmail.com>
2022-08-27 07:20:24 -0700

    debug fetch

--- javascript.js
+++ javascript.js
@@ -9,114 +9,103 @@
 /*6. retrieve data from local storage (getItem) if user clicks same city again
 */
 
-
 var apiKey = "355c56b4969485a6f6e12d1c5ad93d88";
 var searchFormEl = $("#submit-button");
-var citiesSearched = [];
+var citiesSearched = {};
 
+//searchFormEl.on("click", handleSearchFormSubmit());
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-  var citySelected = document.GetElementById("inputcity").value;
-  citySelected = citySelected[0].toUpperCase() + citySelected.slice(1).toLowerCase();
-
-
-//add instructions here to retrieve data from localStorage if this city has previously been selected
-//for (i = 0; i < citiesSearched.length; i++) {
-//if (citySelected = citiesSearched(i)) {
-//  getWeather();
-// } else { fetchCoordinates(); }
-
-
-function fetchCoordinates() {
-
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +
-citySelected + "&limit=1&appid=" + apiKey)
-
-.then(function (response) {
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function (data) {
-      var { lat , lon } = data;
-      console.log(data);  
-});
-} else {
-  alert('Error: ' + response.statusText);
-}
-})
-.catch(function (error) {
-alert('Unable to connect to WeatherMap API');
-});
-};
-}
-
-function getWeather() {
-  fetch("https://api.openweathermap.org/data/3.0/onecall?" +
-  "lat=" + lat + "&lon=" + lon + "units=imperial&appid=" + apiKey)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);  
-  });
-  } else {
-    alert('Error: ' + response.statusText);
-  }
-  })
-  .catch(function (error) {
-  alert('Unable to connect to WeatherMap API');
-  });
-
-  var dayname1 = new Date(value.dt * 1000).toLocaleDateString();
-
-  $("#city").innerHTML = citySelected;
-  $("#currentdate").innerHTML = dayname1;
-  $("#weathericon").innerHTML = response.weather.icon;
-  $("#temp").innerHTML = (response.current.temp + "  degrees");
-  $("#wind").innerHTML = (response.current.wind_speed  + "  MPH");
-  $("#humidity").innerHTML = (response.current.humidity + "%");
-  $("#uvIndex").innerHTML = response.current.uvi;
-}
-
-
-function getForecast() {
+//function handleSearchFormSubmit() {
+  // event.preventDefault();
+ var citySelected = $("#inputcity").value;
   
-  fetch("api.openweathermap.org/data/2.5/forecast?q=" + "lat=" + lat +
-  "&lon=" + lon + "units=imperial&appid=" + apiKey)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);  
-      });   
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-    })
-    .catch(function (error) {
-    alert('Unable to connect to WeatherMap API');
-    });
+  //add instructions here to retrieve data from localStorage if this city has previously been selected
+  //cit
+  //if (citySelected = citiesSearched(i)) {
+  //  getWeather(citySelected);
+  // } else { fetchCoordinates(citySelected); }
 
-        var forecastEl = document.getElementsByClassName("forecast");   //write the 5 forecast blocks to screen
-        var fday = "";
-           for (i = 0; i < 5; i++) {
+  //function fetchCoordinates(citySelected) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" + citySelected +
+        "&appid=" + apiKey) 
+        .then(response => response.json())
+        .then (data => console.log(data))
+  //    .then(function(response)
+  //        console.log(response);
+  //          var lat = response.data.coord.lat;
+  //          var lon = response.data.coord.lon;
+  //          console.log(lat, lon);
+  //    }
+  //}
+  //fetchCoordinates(citySelected);
+  //getWeather();
 
-              var dayname2 = new Date(value.dt * 1000).toLocaleDateString();
-              var icon = value.weather.icon;
-              var temp = value.temp.day;
-              fday = `<div class= "card  bg-primary text-white forecast-day">
-                <p> class= "card-text" ${"#currentdate" + i}</p>
-                <p><span class= "card-text" ${"#weathericon" + i}></span></p>
-                <p> class= "card-text" ${"#temp" + i}</p>
-                <p> class= "card-text" ${"#wind" + i}</p>
-                <p> class= "card-text" ${"#humidity" + i}</p>
-              </div>`;
-            }
-      
-      }
-        
+
+
+
+//function getWeather() {
+ // fetch(
+ //   "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + "34.2257" +"&lon=" + "-77.9447" + "units=imperial&appid=" + apiKey + "&cnt=1")
+
+//.then(function (response) {
+// console.log(response);        
+//})
+//}
+//
+//
+ // var dayname1 = new Date(value.dt * 1000).toLocaleDateString();
+//
+ // $("#city").innerHTML = citySelected;
+ // $("#currentdate").innerHTML = dayname1;
+ // $("#weathericon").innerHTML = response.weather.icon;
+ // $("#temp").innerHTML = response.current.temp + "  degrees";
+ // $("#wind").innerHTML = response.current.wind_speed + "  MPH";
+ // $("#humidity").innerHTML = response.current.humidity + "%";
+ // $("#uvIndex").innerHTML = response.current.uvi;
+//}
+
+
+// function getForecast() {
+//   fetch(
+//     "api.openweathermap.org/data/2.5/forecast?q=" +
+//       "lat=" +
+//       lat +
+//       "&lon=" +
+//       lon +
+//       "units=imperial&appid=" +
+//       apiKey
+//   )
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//         });
+//       } else {
+//         alert("Error: " + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert("Unable to connect to WeatherMap API");
+//     });
+
+//   var forecastEl = document.getElementsByClassName("forecast"); //write the 5 forecast blocks to screen
+//   var fday = "";
+//   for (i = 0; i < 5; i++) {
+//     var dayname2 = new Date(value.dt * 1000).toLocaleDateString();
+//     var icon = value.weather.icon;
+//     var temp = value.temp.day;
+//     fday = `<div class= "card  bg-primary text-white forecast-day">
+//                 <p> class= "card-text" ${"#currentdate" + i}</p>
+//                 <p><span class= "card-text" ${"#weathericon" + i}></span></p>
+//                 <p> class= "card-text" ${"#temp" + i}</p>
+//                 <p> class= "card-text" ${"#wind" + i}</p>
+//                 <p> class= "card-text" ${"#humidity" + i}</p>
+//               </div>`;
+//   }
+// }
+
 //var bodyContentEl = document.createElement('li');                        add city to page under search box
 //bodyContentEl.classList.add( 'bg-secondary', 'text-white', 'rounded');
 //bodyContentEl.innerHTML = citySelected + '<br/>';
